300dd6a5ea60b65302c3c87857de66a0167003c4
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-05-07 12:48:33 +0900

    240507 update schedule

--- src/main/java/com/rod/api/schedule/repository/ScheduleDAO.java
+++ src/main/java/com/rod/api/schedule/repository/ScheduleDAO.java
@@ -4,8 +4,11 @@ package com.rod.api.schedule.repository;
 import com.rod.api.schedule.model.ScheduleDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ScheduleDAO {
 
     List<ScheduleDTO> getAllSchedules();
+
+    List<ScheduleDTO> getProblem23(String startDate, String endDate);
 }
--- src/main/java/com/rod/api/schedule/repository/ScheduleDAOImpl.java
+++ src/main/java/com/rod/api/schedule/repository/ScheduleDAOImpl.java
@@ -2,7 +2,9 @@ package com.rod.api.schedule.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.rod.api.schedule.model.QSchedule;
+import com.rod.api.schedule.model.QScheduleDTO;
 import com.rod.api.schedule.model.ScheduleDTO;
+import com.rod.api.stadium.model.QStadium;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,28 +16,22 @@ public class ScheduleDAOImpl implements ScheduleDAO{
 
     private final JPAQueryFactory factory;
 
-
     @Override
     public List<ScheduleDTO> getAllSchedules() {
-        return factory.select(
-                        QSchedule.schedule.id,
-                        QSchedule.schedule.scheDate,
-                        QSchedule.schedule.gubun,
-                        QSchedule.schedule.hometeamId,
-                        QSchedule.schedule.awayteamId,
-                        QSchedule.schedule.homeScore,
-                        QSchedule.schedule.awayScore
+        return factory.select(new QScheduleDTO(QSchedule.schedule.id, QSchedule.schedule.scheDate, QSchedule.schedule.gubun,
+                        QSchedule.schedule.hometeamId, QSchedule.schedule.awayteamId, QSchedule.schedule.homeScore,
+                        QSchedule.schedule.awayScore, QSchedule.schedule.stadium.id)
                 ).from(QSchedule.schedule)
-                .fetch()
-                .stream()
-                .map(tuple -> ScheduleDTO.builder()
-                        .id(tuple.get(QSchedule.schedule.id))
-                        .scheDate(tuple.get(QSchedule.schedule.scheDate))
-                        .gubun(tuple.get(QSchedule.schedule.gubun))
-                        .hometeamId(tuple.get(QSchedule.schedule.hometeamId))
-                        .awayteamId(tuple.get(QSchedule.schedule.awayteamId))
-                        .homeScore(tuple.get(QSchedule.schedule.homeScore))
-                        .awayScore(tuple.get(QSchedule.schedule.awayScore))
-                        .build()).toList();
+                .fetch();
+    }
+
+    @Override
+    public List<ScheduleDTO> getProblem23(String startDate, String endDate) {
+        return factory.select(new QScheduleDTO(QSchedule.schedule.stadium.stadiumName))
+                .from(QSchedule.schedule)
+                .leftJoin(QSchedule.schedule.stadium, QStadium.stadium)
+                .where(QSchedule.schedule.scheDate.between(startDate, endDate))
+                .groupBy(QStadium.stadium.stadiumName)
+                .fetch();
     }
 }
